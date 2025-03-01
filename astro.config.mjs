62053bf65043496a26f6a3c1958551217bc837a5
G4T0L <jcastroba@unal.edu.co>
2025-03-01 15:38:06 -0500

    Add vercel server side functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 import solid from '@astrojs/solid-js';
 import dotenv from 'dotenv';
-import vercel from '@astrojs/vercel';
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // Cargar variables de entorno desde .env
 dotenv.config();
 
 export default defineConfig({
   integrations: [tailwind(), solid()],
-  adapter: vercel(),
   output: 'server',
+  adapter: vercelServerless({}),
 });
 
